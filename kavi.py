4c7b85c83179433a11463dbec86babd91f74cc8f
Mohanakrishnan <mohankrishsairamit06@gmail.com>
2023-12-12 07:53:56 +0530

    Create basic GUI

---
+++ kavi.py
@@ -0,0 +1,9 @@
+import PySimpleGUI as sg
+
+label = sg.Text("Type in a to-do")
+Input_box = sg.InputText(tooltip="Enter a Todo")
+add_button = sg.Button("Add")
+
+window = sg.Window('My todo App', layout=[[label], [Input_box,add_button]])
+window.read()
+window.close()
