5427163068aeffd55efd56bb2ceed95a8be2ac2e
Mohanakrishnan <mohankrishsairamit06@gmail.com>
2023-12-14 07:55:28 +0530

    Initial commit

--- kavi.py
+++ kavi.py
@@ -4,6 +4,7 @@ label = sg.Text("Type in a to-do")
 Input_box = sg.InputText(tooltip="Enter a Todo")
 add_button = sg.Button("Add")
 
-window = sg.Window('My todo App', layout=[[label], [Input_box,add_button]])
+window = sg.Window('My todo App', layout=[[label, Input_box,add_button]])
 window.read()
+print("Hello")
 window.close()
